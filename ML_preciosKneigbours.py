000454be2f103c14856914e3bb75ee6257b4c015
Nahuel Alejandro <nahuelespechealc@gmail.com>
2026-06-05 15:15:38 +0200

    FinalModel

--- ML_preciosKneigbours.py
+++ ML_preciosKneigbours.py
@@ -1,24 +1,41 @@
 #imports
-from sklearn.neighbors import KNeighborsRegressor
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.datasets import fetch_california_housing
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import GridSearchCV
 
 
 #importar datos
 X, y = fetch_california_housing(return_X_y=True) 
+#x = datos
+#y = target
 
-
+#pipeline
 pipe = Pipeline([
     ("Scale", StandardScaler()),
     ("Model", KNeighborsRegressor())
     ])
 
-pipe.fit(X, y)
-pred = pipe.predict(X)
 
+#pipe.get_params()
+
+mod = GridSearchCV(estimator=pipe,
+             param_grid={'Model__n_neighbors': [1,2,3,4,5,6,7,8,9,10]},
+             cv=3)
+
+#Guardamos predicciones
+mod.fit(X, y)
+
+print(pd.DataFrame(mod.cv_results_)) 
+
+best_model = mod.best_estimator_
+pred = best_model.predict(X)
+
+#Mostramos gráfica por pantalla
 plt.scatter(y, pred)
 plt.xlabel("Real")
 plt.ylabel("Predicción")
